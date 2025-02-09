90b20f3d467aa079b6a2530224963a40c073977d
yuanqh23 <soverreigndragon@gmail.com>
2025-02-09 13:03:54 +0800

    do part of the script

--- script/validation.js
+++ script/validation.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+function validate() {
+	let inputFullName = document.getElementById("fullName");
+	let inputEmail = document.getElementById("email");
+	let inputGitHubName = document.getElementById("githubName");
+	
+	
+	// validating fullName
+	if() {
+	   
+	   }
+}
